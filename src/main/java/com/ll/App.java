3b457b7169ce80bda84de8822693f927927e8d24
astral0525 <astral0525@gmail.com>
2023-11-27 10:23:07 +0900

    step1 complete

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -4,11 +4,17 @@ import java.util.Scanner;
 
 class App {
     void run() {
-        System.out.println("== 명언 앱 ==");
-        System.out.printf("명령) ");
-        Scanner scanner = new Scanner(System.in);
-        String cmd = scanner.nextLine();
+        while (true) {
+            System.out.println("== 명언 앱 ==");
+            System.out.printf("명령) ");
+            Scanner scanner = new Scanner(System.in);
+            String cmd = scanner.nextLine();
 
-        System.out.printf("입력하신 명령 : %s\n", cmd);
+            if (cmd.equals("종료")) {
+                break;
+            }
+
+            System.out.printf("입력하신 명령 : %s\n", cmd);
+        }
     }
 }
