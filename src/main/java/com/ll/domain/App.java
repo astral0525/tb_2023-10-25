e263423aa5d40d6b8c36eb2b4b2cde7c62fd968d
astral0525 <astral0525@gmail.com>
2023-11-27 22:35:12 +0900

    auto make data for test

--- src/main/java/com/ll/domain/App.java
+++ src/main/java/com/ll/domain/App.java
@@ -15,7 +15,22 @@ public class App {
         quotations = new ArrayList<>();
         scanner = new Scanner(System.in);
 
+        initTestData();
+
     }
+    private void initTestData(){
+        for(int i=0; i<10; i++){
+            lastQuotationId++;
+            int id = lastQuotationId;
+            String content = "명언" + id;
+            String authorName = "작가"+ id;
+
+            Quotation quotation = new Quotation(id, content, authorName);
+            quotations.add(quotation);
+        }
+
+    }
+
 
     public void run() {
         while (true) {
