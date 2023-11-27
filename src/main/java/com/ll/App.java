fb55c422b5283d5f61510554a902f1cc93b564bc
astral0525 <astral0525@gmail.com>
2023-11-27 10:40:42 +0900

    step2 complete

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -5,16 +5,22 @@ import java.util.Scanner;
 class App {
     void run() {
         while (true) {
-            System.out.println("== 명언 앱 ==");
             System.out.printf("명령) ");
             Scanner scanner = new Scanner(System.in);
             String cmd = scanner.nextLine();
 
             if (cmd.equals("종료")) {
                 break;
+            } else if (cmd.equals("등록")) {
+                System.out.printf("명언 : ");
+                String content = scanner.nextLine();
+
+                System.out.printf("작가 : ");
+                String authorName = scanner.nextLine();
+
+                System.out.printf("명언 : %s, 작가 : %s\n", content, authorName);
             }
 
-            System.out.printf("입력하신 명령 : %s\n", cmd);
         }
     }
 }
--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -2,6 +2,7 @@ package com.ll;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("== 명언 앱 ==");
         new App().run();
     }
 }
